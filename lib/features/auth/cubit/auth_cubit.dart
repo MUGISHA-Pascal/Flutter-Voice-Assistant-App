f8ba9d4feb573d46fae7e678ef358e4a58c3bd75
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2025-08-11 19:09:31 +0200

    feat : application auth cubit and states setup

---
+++ lib/features/auth/cubit/auth_cubit.dart
@@ -0,0 +1,68 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_voice_assistant_app/core/models/user_model.dart';
+import 'package:flutter_voice_assistant_app/core/services/token_service.dart';
+import 'package:flutter_voice_assistant_app/features/auth/repository/auth_local_repository.dart';
+import 'package:flutter_voice_assistant_app/features/auth/repository/auth_remote_repository.dart';
+
+part 'auth_state.dart';
+
+class AuthCubit extends Cubit<AuthState> {
+  AuthCubit() : super(AuthInitial());
+  final authRemoteRepository = AuthRemoteRepository();
+  final authLocalRepository = AuthLocalRepository();
+  final TokenService tokenService = TokenService();
+  void getUserData() async {
+    try {
+      emit(AuthLoading());
+      final userModel = await authRemoteRepository.getUserData();
+      if (userModel != null) {
+        await authLocalRepository.insertUser(userModel);
+        emit(AuthLoggedIn(userModel));
+      } else {
+        emit(AuthInitial());
+      }
+    } catch (e) {
+      print(e);
+      emit(AuthInitial());
+    }
+  }
+
+  void signUp({
+    required String name,
+    required String email,
+    required String password,
+  }) async {
+    try {
+      emit(AuthLoading());
+      await authRemoteRepository.signup(
+        name: name,
+        email: email,
+        password: password,
+      );
+
+      emit(AuthSignUp());
+    } catch (e) {
+      emit(AuthError(e.toString()));
+    }
+  }
+
+  void login({required String email, required String password}) async {
+    try {
+      emit(AuthLoading());
+      final userModel = await authRemoteRepository.login(
+        email: email,
+        password: password,
+      );
+
+      if (userModel.token.isNotEmpty) {
+        await tokenService.setToken(userModel.token);
+      }
+
+      await authLocalRepository.insertUser(userModel);
+
+      emit(AuthLoggedIn(userModel));
+    } catch (e) {
+      emit(AuthError(e.toString()));
+    }
+  }
+}
