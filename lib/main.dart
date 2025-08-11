4e3c4a6270618e69b5c56800191a1e09826b538a
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2025-08-11 19:19:51 +0200

    feat : update the application main entry

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_voice_assistant_app/core/theme/pallete.dart';
+import 'package:flutter_voice_assistant_app/features/auth/cubit/auth_cubit.dart';
+import 'package:flutter_voice_assistant_app/features/auth/pages/signup_page.dart';
 import 'package:flutter_voice_assistant_app/features/home/pages/home_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiBlocProvider(
+      providers: [BlocProvider(create: (_) => AuthCubit())],
+      child: const MyApp(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    context.read<AuthCubit>().getUserData();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Flutter Voice Assistant',
       debugShowCheckedModeBanner: false,
-      title: 'Allen',
       theme: ThemeData.light(useMaterial3: true).copyWith(
-        scaffoldBackgroundColor: Pallete.whiteColor,
-        appBarTheme: const AppBarTheme(backgroundColor: Pallete.whiteColor),
+        inputDecorationTheme: InputDecorationTheme(
+          contentPadding: const EdgeInsets.all(27),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.grey.shade300, width: 3),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: const BorderSide(width: 3),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          border: OutlineInputBorder(
+            borderSide: const BorderSide(width: 3),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          errorBorder: OutlineInputBorder(
+            borderSide: const BorderSide(color: Colors.red, width: 3),
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.black,
+            minimumSize: const Size(double.infinity, 60),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15),
+            ),
+          ),
+        ),
       ),
-      home: const HomePage(),
+      home: BlocBuilder<AuthCubit, AuthState>(
+        builder: (context, state) {
+          if (state is AuthLoggedIn) {
+            return const HomePage();
+          }
+          return const SignupPage();
+        },
+      ),
     );
   }
 }
